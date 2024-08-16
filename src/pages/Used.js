4228d99ceefc15b8810c9c88980aee242432006c
최수연 <tndus5383@gmail.com>
2024-08-16 15:51:51 +0900

    fix: CSS 문제 수정

--- src/pages/Used.js
+++ src/pages/Used.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../css/Post.css';
+import '../css/Used.css';
 import { FadeLoader } from 'react-spinners';
 import { ShoppingBag } from 'lucide-react';
 
